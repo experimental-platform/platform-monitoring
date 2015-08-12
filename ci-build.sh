c17d5fea1bf1e8f8d59573f845320c4f086dd547
Hinnerk Haardt <haardt@information-control.de>
2015-08-12 11:43:09 +0200

    Initial CI configuration.

--- ci-build.sh
+++ ci-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# THIS ONLY WORK IN OUR CI!
 
-docker run --rm -v /data/jenkins/jobs/${JOB_NAME}/workspace:/usr/src/monitoring -w /usr/src/monitoring golang:1.4 /bin/bash -c 'go get -d && go build -v'
+SRC_PATH=$(pwd)
+
+docker run -v ${SRC_PATH}:/usr/src/monitoring -w /usr/src/monitoring golang:1.4 /bin/bash -c 'go get -d && go build -v'
