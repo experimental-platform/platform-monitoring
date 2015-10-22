a52dcfb298405ee23a2fe7368cb69418a895906d
Kamil Domański <kamil@domanski.co>
2015-10-22 22:49:49 +0200

    fix CI build

--- ci-build.sh
+++ ci-build.sh
@@ -4,4 +4,7 @@ set -e
 SRC_PATH=$(pwd)
 PROJECT_NAME="github.com/experimental-platform/platform-monitoring"
 
-docker run -v "${SRC_PATH}:/go/src/$PROJECT_NAME" -w "/go/src/$PROJECT_NAME" -e GO15VENDOREXPERIMENT=1 golang:1.5 /bin/bash -c './install-glide.sh && glide up && go build -v'
+export GO15VENDOREXPERIMENT=1
+./install-glide-v1.sh
+cp $HOME/bin/glide .
+docker run -v "${SRC_PATH}:/go/src/$PROJECT_NAME" -w "/go/src/$PROJECT_NAME" -e GO15VENDOREXPERIMENT=1 golang:1.5 /bin/bash -c './glide up && go build -v'
